f5bd4611f397e69657f2757cb4c7b0a6d22f0154
sham-jag <55582175+sham-jag@users.noreply.github.com>
2020-12-09 00:30:49 +0530

    idk

--- royal/admin.py
+++ royal/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from . import models
-admin.site.register(models.info)
 admin.site.register(models.customer)
 admin.site.register(models.comp_branch)
 admin.site.register(models.car_model)
--- royal/models.py
+++ royal/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class info(models.Model):
-    login_id=models.IntegerField(null=True)
-    name=models.CharField(max_length=50, verbose_name='username', default='DEFAULT VALUE')
-    log_p=models.CharField(max_length=20, verbose_name='login_password', blank=True)
+
 
 class customer(models.Model):
     customer_id=models. IntegerField(null=True)
